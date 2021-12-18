13d937c66c5f965d94d5d86d48d6b911029b4d11
Ismael <40826314+IsmaelHG@users.noreply.github.com>
2021-12-18 09:11:56 +0100

    *Pequeños cambios*

---
+++ app/src/main/java/cat/urv/miv/mivandroid3d/Light.java
@@ -0,0 +1,51 @@
+package cat.urv.miv.mivandroid3d;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Light {
+	ByteBuffer vtbb;
+	FloatBuffer posicio;
+	FloatBuffer ambient;
+	FloatBuffer difuse;
+	FloatBuffer specular;
+	GL10 gl;
+	int lightid;
+
+	public Light(GL10 gl, int lightid) {
+		this.gl = gl;
+		this.lightid = lightid;
+
+    }
+
+	//To enable and disable the light
+	public void enable() {gl.glEnable(lightid);}
+	public void disable() {gl.glDisable(lightid);}
+
+	//To position the light
+	public void setPosition(float[] pos) {
+		this.posicio = FloatBuffer.wrap(pos);
+		this.setPosition();
+	}
+
+	public void setPosition() {
+		gl.glLightfv(lightid,GL10.GL_POSITION, posicio);
+	}
+
+
+
+	//To set the light colors
+	public void setAmbientColor(float[] color) {
+		this.ambient = FloatBuffer.wrap(color);
+		gl.glLightfv(lightid, GL10.GL_AMBIENT, ambient);
+	}
+
+	public void setDiffuseColor(float[] color) {
+		this.difuse = FloatBuffer.wrap(color);
+		gl.glLightfv(lightid, GL10.GL_DIFFUSE, difuse);
+	}
+
+}
